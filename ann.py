5f53eb1b1fdde638391e355d8d304283897b1665
Paul Renauld <paulrenauld27@gmail.com>
2019-11-24 17:54:25 +0100

    working?

--- ann.py
+++ ann.py
@@ -4,10 +4,18 @@ from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
 
+
+def mse(t1, t2):
+  diff = t1 - t2
+  return torch.sum(diff * diff) / diff.numel()
+
+
 if __name__ == '__main__':
-    dataset = pd.read_csv('data/features.csv')
-    y = torch.from_numpy(dataset[['EGID', 'heating', 'cooling']].set_index('EGID').values).long()
-    features = torch.from_numpy(dataset.drop(columns=['heating', 'cooling']).set_index('EGID').values).float()
+    dataset = pd.read_csv('data/features.csv').set_index('EGID')
+    dataset = (dataset - dataset.mean()) / dataset.std()
+    print(dataset)
+    y = torch.from_numpy(dataset[['heating']].values).long()
+    features = torch.from_numpy(dataset.drop(columns=['heating', 'cooling']).values).float()
     n_features = features.shape[1]
     n_output = y.shape[1]
     n_hidden = int((n_features + n_output) / 2)
@@ -17,19 +25,16 @@ if __name__ == '__main__':
                           nn.ReLU(),
                           nn.Linear(n_hidden, n_output),
                           nn.ReLU())
-    criterionH = nn.NLLLoss()
-    criterionC = nn.NLLLoss()
+    # criterionH = nn.NLLLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.003)
     dataloader = DataLoader(list(zip(features, y)), batch_size=16, shuffle=True)
 
-    for e in range(5):
+    for e in range(10):
       loss = 0
       for x, y in dataloader:
         optimizer.zero_grad()
-        output = model(x)
-        print(output)
-        loss = criterionH(output[0], y[0]) + criterionC(output[1], y[1])
-        exit(1)
-    feature_tensor = torch.tensor(features)
-    print(n_features, n_output, n_hidden)
-    print(feature_tensor)
+        outputH = model(x)
+        loss = mse(outputH, y.squeeze(1))
+        loss.backward()
+        print(loss)
+        print(outputH)
