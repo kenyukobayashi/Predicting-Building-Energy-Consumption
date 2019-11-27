ca72321fe0a80e2957a3c15637fc907297429b4b
Paul Renauld <paulrenauld27@gmail.com>
2019-11-27 14:05:37 +0100

    first NN

--- ann.py
+++ ann.py
@@ -1,40 +1,102 @@
 import pandas as pd
 import torch
+import numpy as np
 from torch import nn
 from torch import optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
 
-def mse(t1, t2):
-  diff = t1 - t2
-  return torch.sum(diff * diff) / diff.numel()
+def df_to_tensor(df):
+  return torch.from_numpy(df.values).float()
+
+
+class DataPreProcessor:
+  def __init__(self, df, split_factor=0.75, columns_to_normalize=None):
+    if columns_to_normalize is None:
+      columns_to_normalize = ['heating', 'cooling', 'GASTW', 'GAREA']
+    indices = np.random.permutation(df.shape[0])
+    split = int(df.shape[0] * split_factor)
+    tr = indices[:split]
+    te = indices[split:]
+    train = df.iloc[tr].copy()
+    test = df.iloc[te].copy()
+
+    self.norm_factors = {}
+    for c in columns_to_normalize:
+      col = train[c]
+      self.norm_factors[c] = (col.min(), col.max() - col.min())
+    self.train = DataLoader(PandaDataset(self.normalize(train)), batch_size=16, shuffle=True)
+    self.test = DataLoader(PandaDataset(self.normalize(test)), batch_size=1, shuffle=True)
+
+  def normalize(self, df):
+    for c, (sub, div) in self.norm_factors.items():
+      df[c] = (df[c] - sub) / div
+    return df
+
+  def denormalize(self, df, column='heating'):
+    sub, div = self.norm_factors[column]
+    df = df * div + sub
+    return df
+
+
+class PandaDataset(Dataset):
+    def __init__(self, df):
+      super(PandaDataset, self).__init__()
+      self.n = df.shape[0]
+      self.features = df_to_tensor(df.drop(columns=['heating', 'cooling']))
+      self.labels = df_to_tensor(df[['heating']])
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, index):
+        return self.features[index], self.labels[index]
+
+
+def evaluate(panda_dataset, preprocessor, model):
+  predictions = model(panda_dataset.features).detach().numpy()
+  predictions = preprocessor.denormalize(predictions)
+  expected = preprocessor.denormalize(panda_dataset.labels.numpy())
+  diff = np.abs(predictions - expected) / expected
+  # print(np.concatenate((predictions, expected), axis=1))
+  return diff.mean()
 
 
 if __name__ == '__main__':
     dataset = pd.read_csv('data/features.csv').set_index('EGID')
-    dataset = (dataset - dataset.mean()) / dataset.std()
-    print(dataset)
-    y = torch.from_numpy(dataset[['heating']].values).long()
-    features = torch.from_numpy(dataset.drop(columns=['heating', 'cooling']).values).float()
-    n_features = features.shape[1]
-    n_output = y.shape[1]
+    dataset.dropna(inplace=True)
+
+    n_features = dataset.iloc[1:2].drop(columns=['heating', 'cooling']).shape[1]
+    n_output = 1
     n_hidden = int((n_features + n_output) / 2)
 
-    # Build a feed-forward network
-    model = nn.Sequential(nn.Linear(n_features, n_hidden),
-                          nn.ReLU(),
-                          nn.Linear(n_hidden, n_output),
-                          nn.ReLU())
-    # criterionH = nn.NLLLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.003)
-    dataloader = DataLoader(list(zip(features, y)), batch_size=16, shuffle=True)
+    data = DataPreProcessor(dataset)
 
-    for e in range(10):
-      loss = 0
-      for x, y in dataloader:
+    model = nn.Sequential(nn.Linear(n_features, n_hidden),
+                          nn.Sigmoid(),
+                          nn.Linear(n_hidden, n_hidden),
+                          nn.Sigmoid(),
+                          nn.Linear(n_hidden, n_output),
+                          nn.Sigmoid())
+    criterionH = nn.MSELoss()
+
+    optimizer = optim.SGD(model.parameters(), lr=0.003)
+
+    for j in range(20000):
+      losses = 0
+      for feat, target in data.train:
         optimizer.zero_grad()
-        outputH = model(x)
-        loss = mse(outputH, y.squeeze(1))
+
+        out = model(feat)
+
+        loss = criterionH(out, target)
         loss.backward()
-        print(loss)
-        print(outputH)
+        optimizer.step()
+
+        out_d = data.denormalize(out)
+        target_d = data.denormalize(target)
+        # losses += torch.mean(abs((out_d - target_d) / target_d))
+        losses += loss.item()
+      if j % 100 == 0:
+        print("Step {j}: relative average error on train: {e:.03}%. Training avg loss: {l}"
+              .format(j=j, e=evaluate(data.train.dataset, data, model) * 100, l=losses / len(data.train)))
